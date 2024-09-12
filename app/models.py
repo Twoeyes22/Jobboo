4b84b88a02acfe3dda1570de9a5a1aedc7a008bf
rednine9777 <rednine9777@gmail.com>
2024-09-12 16:30:16 +0900

    [FEAT] 팀생성2 - 팀이름, 팀소개, 팀설명, 팀로고(이미지), 팀 깃허브 #25

--- app/models.py
+++ app/models.py
@@ -16,7 +16,6 @@ class Team(Base):
     t_git = Column(String(255))
 
     users = relationship("User", back_populates="team")
-    upload_file = relationship("UploadFile", back_populates="team", uselist=False)
 
 class User(Base):
     __tablename__ = 'user'
@@ -32,33 +31,9 @@ class User(Base):
     u_image = Column(String(255))
     team = relationship("Team", back_populates="users")
     
-    upload_file = relationship("UploadFile", back_populates="user", uselist=False)
-
-
-# 주요 변경사항: 없음
-# ORM 모델 정의는 동기/비동기와 관계없이 동일
 
 
 
-# Todo: 업로드 파일 미구현
-class UploadFile(Base):
-    __tablename__ = 'upload_file'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    upload_folder = Column(String(255))
-    upload_path = Column(String(255))
-    client_file_name = Column(String(255))
-    ext_file_name = Column(String(50))
-    file_type = Column(String(50))
-    server_file_name = Column(String(255))
-    full_path = Column(String(255))
-
-    # 팀 또는 유저와의 1:1 관계
-    team_id = Column(Integer, ForeignKey('team.t_id', ondelete="SET NULL"), unique=True, nullable=True)
-    user_id = Column(Integer, ForeignKey('user.u_id', ondelete="SET NULL"), unique=True, nullable=True)
-
-    # 양방향 관계 설정
-    team = relationship("Team", back_populates="upload_file")
-    user = relationship("User", back_populates="upload_file")
 
 
