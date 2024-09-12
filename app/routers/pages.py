921368959de890f808531d624c5039922ef936af
KwangJin <warriorkwang@gmail.com>
2024-09-12 15:51:16 +0900

    add user view api

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from models import User, Team
+from database import get_db
 
 # 라우터 객체 생성
 router = APIRouter()
 
 # Jinja2 템플릿 엔진 설정
 templates = Jinja2Templates(directory="templates")
+
 
 # 팀원 정보를 담은 리스트, DB를 사용해보자!
 team_members = [
@@ -30,4 +36,19 @@ async def resume(request: Request, member_name: str):
     # 해당 팀원의 이력서 HTML 파일을 렌더링
     return templates.TemplateResponse(f"resume/{member_name}.html", {"request": request})
 
-## 다른 사람 메인페이지도 추가해보자.
+@router.get("/user/{user_id}")
+async def team_info(user_id: int, db: AsyncSession = Depends(get_db)):
+    query = select(User).where(User.u_id == user_id)
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()  # 유저 정보가 없으면 None 반환
+    
+    if user is None:
+        return {"error": "User not found"}
+
+    return {
+        "u_nickname": user.u_nickname,
+        "u_id": user.u_id,
+        "u_name": user.u_name,
+        "u_email": user.u_email,
+        "u_git": user.u_git
+    }
